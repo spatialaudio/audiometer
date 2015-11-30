3815efa8927f2339efa76c39fea3a7c5c4efe2f2
franzpl <franz.plocksties@uni-rostock.de>
2015-11-30 16:12:16 +0100

    Fix Callback

--- pure_tone.py
+++ pure_tone.py
@@ -1,28 +1,40 @@
+"""Pure tone generation by a Callback-Function."""
+
 import numpy as np
 import sounddevice as sd
 
 fs = 44100
-dur = 5  # seconds
+dur = 3  # seconds
+i = 1
 
 
-def pure_tone(amp, dur, freq, phase, samplerate, **kwargs):
+def pure_tone(amp, dur, freq, samplerate, frames, **kwargs):
+    help_var = frames
+    global i
+    frames = i * frames
+    i += 1
     t = np.arange(dur * samplerate) / samplerate
-    sine = amp * np.sin(2 * np.pi * freq * t + phase)
+    t_frames = t[frames-help_var: frames]
+    sine = amp * np.sin(2 * np.pi * freq * t_frames)
+    sine.shape = -1, 1
+    sine = np.concatenate((sine, sine), axis=1)  # stereo
+    if(frames > len(t)):  # fill up the last frames with zeros
+        sine = np.concatenate((sine, np.zeros((frames-len(t), 2))))
     return sine
 
 
-def get_chunk():
-    data = pure_tone(amp=1.0, dur=dur, freq=1000, phase=0, samplerate=fs)
-    return data
+def get_chunk(frames):
+    chunk = pure_tone(amp=1.0, dur=dur, freq=1000, samplerate=fs,
+                      frames=frames)
+    return chunk
 
 callback_status = sd.CallbackFlags()
 
 
-def callback(indata, frames, time, status):
+def callback(outdata, frames, time, status):
     global callback_status
-    callback_satus |= status
-    chunk = get_chunk()
-    return chunk
+    callback_status |= status
+    outdata[:] = get_chunk(frames)
 
-stream = sd.Stream(samplerate=fs, callback=callback)
-sd.sleep(dur * 1000)
+with sd.OutputStream(samplerate=fs, callback=callback):
+    sd.sleep(dur * 1000)
