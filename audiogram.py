ec3583696494cfab380d9194f2a151431fa74c06
franzpl <franz.plocksties@uni-rostock.de>
2016-01-06 17:18:40 +0100

    Docstring

--- audiogram.py
+++ audiogram.py
@@ -4,17 +4,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def set_points(dBHL_values, ax=None, title=None, **kwargs):
+def set_points(dBHL_values, freqs=None, ax=None, title=None, **kwargs):
+    """Set measuring points.
+
+    Parameters
+    ----------
+    dBHL_values : array_like
+          dBHL-Values Vector
+    freqs : array_like, optional
+          Frequency Vector in Hz
+    ax: plt.Ax, optional
+          Matplotlib Ax to plot on
+    title: str, optional
+         Audiogram Title
+
+    Returns
+    -------
+    plt.Axis
+        Matplotlib axis containing the plot.
+
+    """
     if ax is None:
         ax = plt.gca()
+    if freqs is None:
+        # standard test frequencies DIN EN IEC 60645-1 ch. 6.1.1.1 Tab.2
+        freqs = [125, 250, 500, 1000, 2000, 4000, 8000]
+    if len(dBHL_values) != len(freqs):
+        raise ValueError("dBHL_values and freqs must have the same length")
+    xticks = np.arange(len(freqs))
     ax.set_xlabel("f / Hz")
-    ax.set_ylabel('Sound Intensity (dBHL)')
-    ax.set_xlim([-0.5, 6.5])
+    ax.set_ylabel('Sound Intensity / dBHL')
+    ax.set_xlim([-0.5, xticks[-1] + 0.5])
     ax.set_ylim([-10, 120])
-    plt.setp(ax, xticks=[0, 1, 2, 3, 4, 5, 6],
-             xticklabels=[125, 250, 500, 1000, 2000, 4000, 8000],
+    plt.setp(ax, xticks=xticks,
+             xticklabels=freqs,
              yticks=[-10, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
-             110, 120])
+                     110, 120])
     ax.invert_yaxis()
     color = 'k'
     if title is not None:
@@ -28,12 +53,14 @@ def set_points(dBHL_values, ax=None, title=None, **kwargs):
     ax.plot(dBHL_values, color=color, marker='x', markersize=11,
             markeredgewidth=1)
     ax.grid(True)
+    gridlines = ax.get_xgridlines() + ax.get_ygridlines()
+    for line in gridlines:
+        line.set_linestyle('-')
     return ax
 
 
 # A short script to test the audiogram
 
 dBHL_values = [30, 50, 80, 75, 84, 50, 40]
-
-set_points(dBHL_values, title='Left Ear')
+set_points(dBHL_values, title='Right Ear')
 plt.show()
