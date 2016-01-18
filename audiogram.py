8e6ca8beade1e31c9d40155d3569cb6fb27d9e28
franzpl <franz.plocksties@uni-rostock.de>
2016-01-18 17:00:55 +0100

    New boolean

--- audiogram.py
+++ audiogram.py
@@ -4,8 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def set_points(dBHL_values, freqs=None, ax=None, title=None, masking=False,
-               conduction='air', **kwargs):
+def set_points(dBHL_values, freqs=None, ax=None, earside='left', title=None,
+               masking=False, conduction='air', **kwargs):
     """Set measuring points.
 
     Parameters
@@ -16,6 +16,8 @@ def set_points(dBHL_values, freqs=None, ax=None, title=None, masking=False,
           Frequency Vector in Hz
     ax: plt.Ax, optional
           Matplotlib Ax to plot on
+    earside: str, default='left'
+          'left' or 'right' ear
     title: str, optional
           Audiogram Title
     masking: bool, default=False
@@ -45,48 +47,51 @@ def set_points(dBHL_values, freqs=None, ax=None, title=None, masking=False,
              yticks=[-10, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
                      110, 120])
     ax.invert_yaxis()
-    if title is not None:
-        ax.set_title(title)
-        if title == 'Left Ear':
-            color = 'b'
-            if conduction == 'air' and masking is False:
-                marker = 'x'
-            elif conduction == 'air' and masking is True:
+    ax.set_title(title)
+    if earside == 'left':
+        color = 'b'
+        if conduction == 'air' and not masking:
+            marker = 'x'
+        elif conduction == 'air' and masking:
                 marker = 's'
-            elif conduction == 'bone' and masking is False:
+        elif conduction == 'bone' and not masking:
                 marker = '4'
-            elif conduction == 'bone' and masking is True:
+        elif conduction == 'bone' and masking:
                 marker = '*'
-            else:
-                raise NameError("Conduction has to be 'air' or 'bone'")
-        elif title == 'Right Ear':
-            color = 'r'
-            if conduction == 'air' and masking is False:
-                marker = 'o'
-            elif conduction == 'air' and masking is True:
-                marker = '^'
-            elif conduction == 'bone' and masking is False:
-                marker = '3'
-            elif conduction == 'bone' and masking is True:
-                marker = '8'
-            else:
-                raise NameError("Conduction has to be 'air' or 'bone'")
         else:
-            raise NameError("'Left Ear' or 'Right Ear'?")
-    else:
-        color = 'k'
-        marker = 'p'
-    ax.plot(dBHL_values, color=color, marker=marker, markersize=11,
+            raise NameError("Conduction has to be 'air' or 'bone'")
+    elif earside == 'right':
+        color = 'r'
+        if conduction == 'air' and not masking:
+            marker = 'o'
+        elif conduction == 'air' and masking:
+            marker = '^'
+        elif conduction == 'bone' and not masking:
+            marker = '3'
+        elif conduction == 'bone' and masking:
+            marker = '8'
+        else:
+            raise NameError("Conduction has to be 'air' or 'bone'")
+    elif not earside == 'right' or not earside == 'left':
+        raise NameError("'left' or 'right'?")
+    lines = ax.plot(dBHL_values, color=color, marker=marker, markersize=11,
             markeredgewidth=1, fillstyle='none')
     ax.grid(True)
     gridlines = ax.get_xgridlines() + ax.get_ygridlines()
     for line in gridlines:
         line.set_linestyle('-')
-    return ax
+    return lines
 
 
 # A short script to test the audiogram
 
-dBHL_values = [30, 50, 80, 75, 84, 50, 40]
-set_points(dBHL_values, title='Right Ear')
+dBHL_values_right = [35, 45, 70, 75, 84, 60,30]
+dBHL_values_left = [30, 50, 80, 75, 84, 50, 40]
+
+f,(ax1,ax2) = plt.subplots(ncols=2)
+set_points(dBHL_values_right, earside='right', title='Right Ear', ax=ax1)
+
+set_points(dBHL_values_left, earside='left', title='Left Ear', ax=ax2)
 plt.show()
+
+
