a0cbe3662fad0aadf4ef2f8cbe67bb8ea3e80529
franzpl <franz.plocksties@uni-rostock.de>
2016-01-26 15:37:35 +0100

    more functionality

--- test_script.py
+++ test_script.py
@@ -2,22 +2,30 @@ import tone_generator
 import time
 import responder
 
+
 device = 0
+duration = 2 # duration of tone in seconds
 
-audio = tone_generator.AudioStream(device) 
-res = responder.MouseResponder()
-audio.start(1000,0.1)
-print("waiting")
-res.wait_for_click() # blocking function
-print("Received click")
-audio.stop()
-res.stop()
+audio = tone_generator.AudioStream(device)  # open stream
+rpd = responder.MouseResponder()  # initialize MouseResponder
+audio.start(1000, 0.03)  # start first tone
+print("wait for clicking1")
+click = rpd.wait_for_click(duration)  # you have 2 seconds to click 
 
-time.sleep(2)
+audio.stop()  # stop tone
+rpd.stop()  # stop responder
+rpd = responder.MouseResponder()  # reinitialize MouseResponder
 
-res = responder.MouseResponder()
-audio.start(10000,0.5)
-print("waiting")
-res.wait_for_click() # blocking function
-res.stop()
-audio.stop()
+if click is True:  # if tone is audible (clicking)..increase next tone
+    audio.start(1000, 0.05)  # start increased tone
+    print("wait for clicking2")
+    click = rpd.wait_for_click(duration)
+    audio.stop()
+    rpd.stop()
+
+else:  # if tone is not audible (not clicking)...decrease next tone
+    audio.start(1000, 0.01)
+    print("wait for clicking3")
+    click = rpd.wait_for_click(duration)
+    audio.stop()
+    rpd.stop()
