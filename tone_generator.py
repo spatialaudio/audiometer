ca6b0fb8c9cb0c5f139a2d0ae506426f8777da7a
franzpl <franz.plocksties@uni-rostock.de>
2016-03-01 17:49:25 +0100

    remove global vars,add earside, fadein fadeout

--- tone_generator.py
+++ tone_generator.py
@@ -4,84 +4,75 @@ import numpy as np
 import sounddevice as sd
 
 samplerate = 44100
-i = 1
-
-
-playing = False
 callback_status = sd.CallbackFlags()
 
 
-def pure_tone(frames, freq, gain):
-    help_var = frames
-    global i
-    frames = i * frames
-    i += 1
-    k = np.arange(frames) / samplerate
-    k_frames = k[frames-help_var: frames]
-    sine = gain * np.sin(2 * np.pi * freq * k_frames)
+def pure_tone(frames, freq, gain_db, i, timeout, earside):
+    packet = i * frames
+    k = np.arange(packet) / samplerate
+    k_frames = k[packet-frames: packet]
+    sine = _db_to_lin(gain_db) * np.sin(2 * np.pi * freq * k_frames)
+    if i == 1:
+        sine = _fadein(sine, frames)
+    if (i+1) * frames > timeout * samplerate:
+        sine = _fadeout(sine, frames)
     sine.shape = -1, 1
-    sine = np.concatenate((sine, sine), axis=1)
+    zeros = np.zeros(len(sine))
+    zeros.shape = -1, 1
+    if earside == 'left':
+        sine = np.concatenate((sine, zeros), axis=1)
+    elif earside == 'right':
+        sine = np.concatenate((zeros, sine), axis=1)
+    elif earside is None:
+        sine = np.concatenate((sine, sine), axis=1)
+    else:
+        raise NameError("'left' or 'right'?")
     return sine
 
 
 class AudioStream:
     def __init__(self, device):
         self._stream = sd.OutputStream(device=device,
-                                       callback=self.callback, channels=2)
-        self._freq = 1000
-        self._gain = 0.1
+                                       callback=self._callback, channels=2)
+        self._playing = False
         self._stream.start()
-     
-    def callback(self, outdata, frames, time, status):
+
+    def _callback(self, outdata, frames, time, status):
         global callback_status
         callback_status |= status
-        if playing:
-            outdata[:] = pure_tone(frames, self._freq, self._gain)
+        if self._playing:
+            outdata[:] = pure_tone(frames, self._freq, self._gain_db,
+                                   self._i, self._timeout, self._earside)
+            self._i += 1
         else:
             outdata.fill(0)
 
-    def start(self, freq, gain):
+    def start(self, freq, gain_db, timeout, earside=None):
         self._freq = freq
-        self._gain = gain
-        global playing
-        playing = True
-    
-    def stop(self):
-        global playing
-        playing = False
+        self._gain_db = gain_db
+        self._i = 1
+        self._earside = earside
+        self._timeout = timeout
+        self._playing = True
 
-    def close_stream(self):
+    def stop(self):
+        self._playing = False
+
+    def close(self):
         self._stream.stop()
 
 
-
-#~ class AudioStream:
-    #~ def __init__(self, device):
-        #~ def callback(outdata, frames, time, status):
-            #~ global callback_status
-            #~ callback_status |= status
-            #~ if playing:
-                #~ outdata[:] = pure_tone(frames, self._freq, self._gain)
-            #~ else:
-                #~ outdata.fill(0)
-        #~ self._stream = sd.OutputStream(device=device, callback=callback, channels=2)
-        #~ self._freq = 1000
-        #~ self._gain = 0.1
-        #~ self._stream.start()
-    #~ def start(self, freq, gain):
-        #~ self._freq = freq
-        #~ self._gain = gain
-        #~ global playing
-        #~ playing = True
-    #~ def stop(self):
-        #~ global playing
-        #~ playing = False
-    #~ def close_stream(self):
-        #~ self._stream.stop()
+def _db_to_lin(db_value):
+    return 10 ** (db_value / 20)
 
 
+def _fadein(sine, frames):
+    fade = np.arange(frames) / frames
+    sine *= np.power(0.1, (1 - fade) * 5)
+    return sine
 
- #~ 
-#~ with responder.MouseResponder():
-    #~ with sd.OutputStream(device=0, callback=callback): 
-        #~ sd.sleep(dur * 1000)
+
+def _fadeout(sine, frames):
+    fade = np.arange(frames) / frames
+    sine *= np.power(0.1, (1 - fade) * 5)[::-1]
+    return sine
