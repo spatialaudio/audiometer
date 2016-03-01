aa0f184e1ac5506ad265ee9f7a63f52073816041
franzpl <franz.plocksties@uni-rostock.de>
2016-03-01 17:50:26 +0100

    rename duration, add earside

--- test_script.py
+++ test_script.py
@@ -4,28 +4,30 @@ import responder
 
 
 device = 0
-duration = 2 # duration of tone in seconds
+timeout = 2  # seconds
 
 audio = tone_generator.AudioStream(device)  # open stream
 rpd = responder.MouseResponder()  # initialize MouseResponder
-audio.start(1000, 0.03)  # start first tone
+audio.start(500, -30, timeout, earside='right')  # start first tone
 print("wait for clicking1")
-click = rpd.wait_for_click(duration)  # you have 2 seconds to click 
+click = rpd.wait_for_click(timeout)  # you have 2 seconds to click
 
 audio.stop()  # stop tone
-rpd.stop()  # stop responder
+rpd.close()  # close responder
 rpd = responder.MouseResponder()  # reinitialize MouseResponder
 
-if click is True:  # if tone is audible (clicking)..increase next tone
-    audio.start(1000, 0.05)  # start increased tone
+if click:  # if tone is audible (clicking)..increase next tone
+    audio.start(3000, -26, timeout, earside='left')  # start increased tone
     print("wait for clicking2")
-    click = rpd.wait_for_click(duration)
+    click = rpd.wait_for_click(timeout)
     audio.stop()
-    rpd.stop()
+    rpd.close()
 
 else:  # if tone is not audible (not clicking)...decrease next tone
-    audio.start(1000, 0.01)
+    audio.start(1000, -40, timeout, earside='left')
     print("wait for clicking3")
-    click = rpd.wait_for_click(duration)
+    click = rpd.wait_for_click(timeout)
     audio.stop()
-    rpd.stop()
+    rpd.close()
+
+audio.close()
