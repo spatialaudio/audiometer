f5dd3cc9bd11fe21a5c19b5cb50ec5e9c6673673
franzpl <franz.plocksties@uni-rostock.de>
2016-03-10 23:35:15 +0100

    Add Fade-in,Fade-out

--- tone_generator.py
+++ tone_generator.py
@@ -2,77 +2,95 @@
 
 import numpy as np
 import sounddevice as sd
+import time
+import logging
 
 samplerate = 44100
-callback_status = sd.CallbackFlags()
-
-
-def pure_tone(frames, freq, gain_db, i, timeout, earside):
-    packet = i * frames
-    k = np.arange(packet) / samplerate
-    k_frames = k[packet-frames: packet]
-    sine = _db_to_lin(gain_db) * np.sin(2 * np.pi * freq * k_frames)
-    if i == 1:
-        sine = _fadein(sine, frames)
-    if (i+1) * frames > timeout * samplerate:
-        sine = _fadeout(sine, frames)
-    sine.shape = -1, 1
-    zeros = np.zeros(len(sine))
-    zeros.shape = -1, 1
-    if earside == 'left':
-        sine = np.concatenate((sine, zeros), axis=1)
-    elif earside == 'right':
-        sine = np.concatenate((zeros, sine), axis=1)
-    elif earside is None:
-        sine = np.concatenate((sine, sine), axis=1)
-    else:
-        raise NameError("'left' or 'right'?")
-    return sine
 
 
 class AudioStream:
-    def __init__(self, device):
+    def __init__(self, device, fade_in, fade_out):
         self._stream = sd.OutputStream(device=device,
                                        callback=self._callback, channels=2)
+        self._audio_stop = False
+        self._inlength = np.round(_seconds_to_samples(fade_in /
+                                                      1000)).astype(int)
+        self._outlength = np.round(_seconds_to_samples(fade_out /
+                                                       1000)).astype(int)
         self._playing = False
+        self._running = False
+        self._callback_status = sd.CallbackFlags()
         self._stream.start()
 
     def _callback(self, outdata, frames, time, status):
-        global callback_status
-        callback_status |= status
+        self._callback_status |= status
         if self._playing:
-            outdata[:] = pure_tone(frames, self._freq, self._gain_db,
-                                   self._i, self._timeout, self._earside)
-            self._i += 1
+            outdata[:] = self._pure_tone(frames, self._freq, self._gain,
+                                         self._earside)
+            if self._running:
+                self._playing = False
         else:
             outdata.fill(0)
 
-    def start(self, freq, gain_db, timeout, earside=None):
+    def _pure_tone(self, frames, freq, gain, earside):
+        k = np.arange(self._index, self._index + frames) / samplerate
+        sine = gain * np.sin(2 * np.pi * freq * k)
+        if self._index < self._inlength:  # fade in
+            k = k * samplerate / self._inlength
+            if self._inlength in k * self._inlength:
+                sine[:self._inlength] *= k[:self._inlength]
+            else:
+                sine *= k
+        if self._audio_stop:  # fade out
+            k = np.arange(self._outlength) / self._outlength
+            k = k[::-1]
+            self._end += frames
+            if self._end > self._outlength:
+                self._end = self._outlength
+                #  sine = np.append(sine[:self._end - self._start],
+                #                   np.zeros(frames -
+                #                   len(sine[:self._end - self._start])))
+                sine = np.zeros(frames)
+                self._running = True
+            else:
+                sine *= k[self._start:self._end]
+            self._start += frames
+        self._index += frames
+        sine.shape = -1, 1
+        zeros = np.zeros(len(sine))
+        zeros.shape = -1, 1
+        if earside == 'left':
+            sine = np.column_stack((sine, zeros))
+        elif earside == 'right':
+            sine = np.column_stack((zeros, sine))
+        else:
+            raise ValueError("'left' or 'right'?")
+        return sine
+
+    def start(self, freq, gain_db, earside=None):
         self._freq = freq
-        self._gain_db = gain_db
-        self._i = 1
+        self._gain = _db_to_lin(gain_db)
         self._earside = earside
-        self._timeout = timeout
+        self._index = 0
+        self._start = 0
+        self._end = 0
+        self._audio_stop = False
+        self._running = False
         self._playing = True
 
-    def stop(self):
-        self._playing = False
+    def stop(self, fade_out):
+        self._audio_stop = True
+        time.sleep(fade_out/1000)
 
     def close(self):
+        if self._callback_status:
+            logging.warning(str(self._callback_status))
         self._stream.stop()
+
+
+def _seconds_to_samples(seconds):
+    return samplerate * seconds
 
 
 def _db_to_lin(db_value):
     return 10 ** (db_value / 20)
-
-
-def _fadein(sine, frames):
-    fade = np.arange(frames) / frames
-    sine *= np.power(0.1, (1 - fade) * 5)
-    return sine
-
-
-def _fadeout(sine, frames):
-    fade = np.arange(frames) / frames
-    sine *= np.power(0.1, (1 - fade) * 5)[::-1]
-    return sine
