7040c4c94048d46272e3881c07e4a0c5037efa74
franzpl <franz.plocksties@uni-rostock.de>
2016-04-12 17:46:31 +0200

    thread-safe, add asserts

--- library/tone_generator.py
+++ library/tone_generator.py
@@ -9,11 +9,12 @@ samplerate = 44100
 
 class AudioStream:
     def __init__(self, device, attack, release):
+        assert attack > 0 and release > 0, (
+            "attack and release have to be positive and different from zero")
         self._stream = sd.OutputStream(device=device,
                                        callback=self._callback, channels=2)
         self._attack = np.round(_seconds2samples(attack / 1000)).astype(int)
         self._release = np.round(_seconds2samples(release / 1000)).astype(int)
-        self._gain = 0
         self._slope = 0
         self._target_gain = 0
         self._last_gain = 0
@@ -25,19 +26,24 @@ class AudioStream:
 
     def _callback(self, outdata, frames, time, status):
         self._callback_status |= status
+        # This is thread-safe, see http://stackoverflow.com/a/17881014/500098:
+        slope, target_gain = self._slope, self._target_gain
         outdata.fill(0)
         k = np.arange(self._index, self._index + frames)
-        ramp = np.arange(frames) * self._slope + self._last_gain + self._slope
-        if self._target_gain >= self._last_gain:
-            self._gain = np.minimum(self._target_gain, np.absolute(ramp))
+        ramp = np.arange(frames) * slope + self._last_gain + slope
+        if slope > 0:
+            gain = np.minimum(target_gain, ramp)
         else:
-            self._gain = np.maximum(self._target_gain, ramp)
-        signal = self._gain * np.sin(2 * np.pi * self._freq * k / samplerate)
+            gain = np.maximum(target_gain, ramp)
+        signal = gain * np.sin(2 * np.pi * self._freq * k / samplerate)
         outdata[:, self._channel] = signal
         self._index += frames
-        self._last_gain = self._gain[-1]
+        self._last_gain = gain[-1]
 
     def start(self, freq, gain_db, earside=None):
+        assert self._target_gain == 0, (
+            "Before calling start(), target_gain has to be zero")
+        assert gain_db != -np.inf, "gain_db must be a finite value"
         self._freq = freq
         self._target_gain = _db2lin(gain_db)
         self._slope = self._target_gain / self._attack
@@ -50,6 +56,8 @@ class AudioStream:
             raise ValueError("'left' or 'right'?")
 
     def stop(self):
+        assert self._target_gain != 0, (
+            "Before calling stop(), target_gain has to be different from zero")
         self._slope = - self._target_gain / self._release
         self._target_gain = 0
 
