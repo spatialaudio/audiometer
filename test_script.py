a1ac0d6fe9d0c6b387fa3c5a2b6b6332fbcabdeb
franzpl <franz.plocksties@uni-rostock.de>
2016-04-13 12:29:32 +0200

    import from audiometer

--- test_script.py
+++ test_script.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
 
-import tone_generator
+from audiometer import tone_generator
+from audiometer import responder
 import time
-import pyxhook_responder
 
 
 device = 0
 timeout = 2  # seconds
-fade_in = 30  # ms
-fade_out = 40  # ms
+attack = 30  # ms
+release = 40  # ms
 
-audio = tone_generator.AudioStream(device, fade_in, fade_out)  # open stream
-rpd = pyxhook_responder.MouseResponder()  # initialize MouseResponder
+audio = tone_generator.AudioStream(device, attack, release)  # open stream
+rpd = responder.MouseResponder()  # initialize MouseResponder
 audio.start(500, -10, earside='right')  # start first tone
 click = rpd.wait_for_click(timeout)  # you have x seconds to click
 audio.stop()  # stop tone
+time.sleep(2)
 if click:
-    frequency, level = 1000, -40
+    frequency, level = 1000, -15
 else:
-    frequency, level = 2000, -25
+    frequency, level = 2000, -20
 audio.start(frequency, level, earside='left')
-
 click = rpd.wait_for_click(timeout)
 audio.stop()
+time.sleep(0.5)
 rpd.close()
 audio.close()
-audio.plotting_output()
