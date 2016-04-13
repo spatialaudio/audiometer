f2bdcec6b63c3176502eb3fc9879477d4919e8d0
franzpl <franz.plocksties@uni-rostock.de>
2016-04-13 13:50:31 +0200

    fix thread-safe again

--- audiometer/tone_generator.py
+++ audiometer/tone_generator.py
@@ -15,8 +15,9 @@ class AudioStream:
                                        callback=self._callback, channels=2)
         self._attack = np.round(_seconds2samples(attack / 1000)).astype(int)
         self._release = np.round(_seconds2samples(release / 1000)).astype(int)
-        self._slope = 0
-        self._target_gain = 0
+        target_gain = 0
+        slope = 0
+        self._mytuple = target_gain, slope
         self._last_gain = 0
         self._freq = 0
         self._channel = 0
@@ -26,9 +27,8 @@ class AudioStream:
 
     def _callback(self, outdata, frames, time, status):
         self._callback_status |= status
-        mytuple = self._slope, self._target_gain
         # This is thread-safe, see http://stackoverflow.com/a/17881014/500098:
-        slope, target_gain = mytuple
+        target_gain, slope = self._mytuple
         outdata.fill(0)
         k = np.arange(self._index, self._index + frames)
         ramp = np.arange(frames) * slope + self._last_gain + slope
@@ -42,25 +42,27 @@ class AudioStream:
         self._last_gain = gain[-1]
 
     def start(self, freq, gain_db, earside=None):
-        assert self._target_gain == 0, (
+        assert self._mytuple[0] == 0, (
             "Before calling start(), target_gain has to be zero")
         assert gain_db != -np.inf, "gain_db has to be a finite value"
         self._freq = freq
-        self._target_gain = _db2lin(gain_db)
-        self._slope = self._target_gain / self._attack
+        target_gain = _db2lin(gain_db)
+        slope = target_gain / self._attack
+        self._mytuple = target_gain, slope
         self._index = 0
         if earside == 'left':
-            self._channel = 1
-        elif earside == 'right':
             self._channel = 0
+        elif earside == 'right':
+            self._channel = 1
         else:
             raise ValueError("'left' or 'right'?")
 
     def stop(self):
-        assert self._target_gain != 0, (
+        assert self._mytuple[0] != 0, (
             "Before calling stop(), target_gain has to be different from zero")
-        self._slope = - self._target_gain / self._release
-        self._target_gain = 0
+        target_gain = 0
+        slope = - self._mytuple[0] / self._release
+        self._mytuple = target_gain, slope
 
     def close(self):
         if self._callback_status:
