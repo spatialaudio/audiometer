4e57835dda3edaa2e25b090a9c07beb64f8d4139
franzpl <franz.plocksties@uni-rostock.de>
2016-04-20 14:38:16 +0200

    add with statement

--- test_script.py
+++ test_script.py
@@ -10,19 +10,15 @@ timeout = 2  # seconds
 attack = 30  # ms
 release = 40  # ms
 
-audio = tone_generator.AudioStream(device, attack, release)  # open stream
-rpd = responder.MouseResponder()  # initialize MouseResponder
-audio.start(500, -10, earside='right')  # start first tone
-click = rpd.wait_for_click(timeout)  # you have x seconds to click
-audio.stop()  # stop tone
-time.sleep(2)
-if click:
-    frequency, level = 1000, -15
-else:
-    frequency, level = 2000, -20
-audio.start(frequency, level, earside='left')
-click = rpd.wait_for_click(timeout)
-audio.stop()
-time.sleep(0.5)
-rpd.close()
-audio.close()
+with tone_generator.AudioStream(device, attack, release) as audio, (
+     responder.MouseResponder()) as rpd:
+    audio.start(500, -40, earside='right')  # start first tone
+    click = rpd.wait_for_click(timeout)  # you have x seconds to click
+    audio.stop()  # stop tone
+    time.sleep(2)
+    if click:
+        frequency, level = 1000, -15
+    else:
+        frequency, level = 2000, -20
+    audio.start(frequency, level, earside='left')
+    click = rpd.wait_for_click(timeout)
