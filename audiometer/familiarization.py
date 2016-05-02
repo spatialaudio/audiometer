a98c9840a0b6e0f7b80bd5fa47a68a080e53aa56
franzpl <franz.plocksties@uni-rostock.de>
2016-05-02 16:05:52 +0200

    rename decrease and increase variables

--- audiometer/familiarization.py
+++ audiometer/familiarization.py
@@ -1,18 +1,19 @@
 """Familiarization. """
 
+start_level = -40  # dB
+decrease_20db = 20  # dB
+increase_10db = 10  # dB
+
 
 def fam(ctrl, freq, earside):
-    start_level = -40  # dB
-    decreasing_tone_fam = 20  # dB
-    increasing_tone_fam = 10  # dB
     click = ctrl._process(freq, start_level, earside)
     current_level = start_level
 
     while not click:  # increasing tone
-        current_level += increasing_tone_fam
+        current_level += increase_10db
         click = ctrl._process(freq, current_level, earside)
     while click:  # decreasing tone
-        current_level -= decreasing_tone_fam
+        current_level -= decrease_20db
         click = ctrl._process(freq, current_level, earside)
 
     ctrl._save_results(current_level, freq, earside, 'Familiarization')
