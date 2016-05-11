076d1c660ac0f1223b530daf7319308fc69d18fe
franzpl <franz.plocksties@uni-rostock.de>
2016-05-11 11:17:26 +0200

    increase/decrease/start_level load from config

--- audiometer/familiarization.py
+++ audiometer/familiarization.py
@@ -1,21 +1,16 @@
 """Familiarization. """
 
-start_level = -40  # dB
-decrease_20db = 20  # dB
-increase_10db = 10  # dB
 
+def fam(ctrl, freq, increase_10db, decrease_20db, start_level_familiar,
+        earside):
+    click = ctrl.process(freq, start_level_familiar, earside)
+    current_level = start_level_familiar
 
-def fam(ctrl, freq, earside):
-    click = ctrl._process(freq, start_level, earside)
-    current_level = start_level
-
-    while not click:  # increasing tone
+    while not click:
         current_level += increase_10db
-        click = ctrl._process(freq, current_level, earside)
-    while click:  # decreasing tone
+        click = ctrl.process(freq, current_level, earside)
+    while click:
         current_level -= decrease_20db
-        click = ctrl._process(freq, current_level, earside)
-
-    ctrl._save_results(current_level, freq, earside, 'Familiarization')
+        click = ctrl.process(freq, current_level, earside)
 
     return current_level
