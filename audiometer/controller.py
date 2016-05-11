0e4343420019a4e1e1d4d058b213d4615dd80a14
franzpl <franz.plocksties@uni-rostock.de>
2016-05-11 11:15:23 +0200

    add config_loader, save results in csv

--- audiometer/controller.py
+++ audiometer/controller.py
@@ -1,63 +1,66 @@
 from audiometer import tone_generator
 from audiometer import responder
-from configparser import ConfigParser
+import csv
 import time
 import random
+from audiometer import config_loader
 
 
 class Controller:
 
     def __init__(self):
-        self._cfg1 = ConfigParser()
-        self._cfg2 = ConfigParser()
-        self._cfg1.read('config.ini')
-        device = int(self._cfg1['Device']['device'])
-        attack = int(self._cfg1['FadeIn/FadeOut Time']['attack'])
-        release = int(self._cfg1['FadeIn/FadeOut Time']['release'])
-        self._timeout = int(self._cfg1['Timeout']['timeout'])
-        responder_device = self._cfg1['Responder']['responder_device']
+        (device, attack, release, self.timeout, self.freqs, self.increase_5db,
+        self.increase_10db, self.decrease_10db, self.decrease_20db,
+        self.start_level_familiar, self.earside_beginning,
+        responder_device) = config_loader.load()
+
+        self._csvfile = open('results.csv', 'w')
+        self._writer = csv.writer(self._csvfile)
+        self._writer.writerow(['Familiarization Result / dB', 'Level / dB',
+                               'Frequency / Hz', 'Earside'])
 
         self._audio = tone_generator.AudioStream(device, attack, release)
-        self._rpd = responder.Responder(self._timeout, responder_device)
+        self._rpd = responder.Responder(self.timeout, responder_device)
 
-    def _freq_list(self):
-        # http://stackoverflow.com/a/30223001
-        str_freqs = self._cfg1.get('Standard Frequencies', 'freqs').split('\n')
-        freq_list = [int(i) for i in str_freqs]
-        return (freq_list[freq_list.index(1000):] +
-        freq_list[:freq_list.index(1000)][::-1])
+    def get_increases_decreases(self):
+        return (self.increase_5db, self.increase_10db, self.decrease_10db,
+                self.decrease_20db)
 
-    def _earside(self):
-        earside_beginning = self._cfg1['Beginning Earside']['earside']
-        if earside_beginning == 'right':
+    def get_start_level_familiar(self):
+        return self.start_level_familiar
+
+    def get_freqs(self):
+        return self.freqs
+
+    def get_earside(self):
+        if self.earside_beginning == 'right':
             earside_contralateral = 'left'
-        elif earside_beginning == 'left':
+        elif self.earside_beginning == 'left':
             earside_contralateral = 'right'
         else:
             raise ValueError("earside_beginning must be 'right' or 'left'")
-        return earside_beginning, earside_contralateral
+        return self.earside_beginning, earside_contralateral
 
-    def _process(self, freq, level, earside):
+    def process(self, freq, level, earside):
         if level >= 0:
-            raise OverflowError("The signal is distorted. Please, repeat "
-            "the hearing test")
+            raise OverflowError("The signal is distorted. Possible causes are "
+            "an incorrect calibration or a severe hearing loss")
         self._audio.start(freq, level, earside)
         click = self._rpd.wait_for_click()
         self._audio.stop()
-        time.sleep(self._timeout + random.random())  # described in chapter 3.1
+        time.sleep(self.timeout + random.random())
         return click
 
-    def _save_results(self, level, freq, earside, section):
-        self._cfg2.read('results.ini')
-        if section not in self._cfg2:
-            self._cfg2.add_section(section)
-        self._cfg2.set(section, 'level_{}Hz_{}'.format(freq, earside),
-            str(level))
-        with open('results.ini', 'w') as self._res:
-            self._cfg2.write(self._res)
+    def save_results(self, familiar_result, level, freq, earside):
+        row = [familiar_result, level, freq, earside]
+        self._writer.writerow(row)
 
-    def _exit(self):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
         time.sleep(0.1)
         self._rpd.close()
         self._audio.close()
+        self._csvfile.close()
 
