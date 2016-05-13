56517139dad799a9764d133823f64431e0273329
franzpl <franz.plocksties@uni-rostock.de>
2016-05-13 16:03:08 +0200

    use argparse

--- audiometer/config_loader.py
+++ audiometer/config_loader.py
@@ -1,51 +1,45 @@
-from configparser import ConfigParser
-import itertools
+import argparse
 
 
-def load():
+class Config():
 
-    try:
-        cfg = ConfigParser()
-        with open('config.ini') as f:
-            # http://stackoverflow.com/a/8555776
-            cfg.read_file(itertools.chain(['[Parameters]'], f))  # Fake Section
+    def __init__(self):
 
-    except EnvironmentError:
-        with open('config_default.ini', 'w') as f_default:
-            cfg.add_section('Parameters')
-            cfg.set('Parameters', 'device', '0')
-            cfg.set('Parameters', 'attack', '30')
-            cfg.set('Parameters', 'release', '40')
-            cfg.set('Parameters', 'responder_device', 'mouse left down')
-            cfg.set('Parameters', 'timeout', '2')
-            cfg.set('Parameters', 'earside', 'right')
-            cfg.set('Parameters', 'calibration_factor', '1')
-            cfg.set('Parameters', 'freqs', ('1000\n1500\n2000\n3000\n4000\n6000'
-                                            '\n8000\n750\n500\n250\n125'))
-            cfg.set('Parameters', 'increase_5db', '5')
-            cfg.set('Parameters', 'increase_10db', '10')
-            cfg.set('Parameters', 'decrease_10db', '10')
-            cfg.set('Parameters', 'decrease_20db', '20')
-            cfg.set('Parameters', 'start_level_familiar', '-40')
+        parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
+        parser.add_argument("-device", help='How to select your soundcard is '
+        'shown in http://python-sounddevice.readthedocs.org/en/0.3.3/'
+        '#sounddevice.query_devices', type=int, default=0)
+        parser.add_argument("-calibration_factor", type=int, default=1)
+        parser.add_argument("-responder_device", type=str,
+                            default="mouse left down")
+        parser.add_argument("-attack", type=int, default=30)
+        parser.add_argument("-release", type=int, default=40)
+        parser.add_argument("-timeout", type=int, default=2)
+        parser.add_argument("-earside", type=str, default="right")
+        parser.add_argument("-small_level_increment", type=int, default=5)
+        parser.add_argument("-large_level_increment", type=int, default=10)
+        parser.add_argument("-small_level_decrement", type=int, default=10)
+        parser.add_argument("-large_level_decrement", type=int, default=20)
+        parser.add_argument("-start_level_familiar", type=int, default=-40)
+        parser.add_argument("-freqs", type=str, default='1000,1500,2000,3000,'
+                            '4000,6000,8000,750,500,250,125')
+        try:
+            args = parser.parse_args(['@config.txt'])
+        except:
+            print("No Config found! Using default values")
+            with open('config_default.txt', 'w') as cfg:
+                cfg.write('')
+                args = parser.parse_args(['@config_default.txt'])
 
-            cfg.write(f_default)
-
-    finally:
-            device = int(cfg['Parameters']['device'])
-            attack = int(cfg['Parameters']['attack'])
-            release = int(cfg['Parameters']['release'])
-            timeout = int(cfg['Parameters']['timeout'])
-            # http://stackoverflow.com/a/30223001
-            freqs = [int(i) for i in cfg.get('Parameters', 'freqs').split('\n')]
-            increase_5db = int(cfg['Parameters']['increase_5db'])
-            increase_10db = int(cfg['Parameters']['increase_10db'])
-            decrease_10db = int(cfg['Parameters']['decrease_10db'])
-            decrease_20db = int(cfg['Parameters']['decrease_20db'])
-            start_level_familiar = int(cfg['Parameters']['start_level_familiar'])
-            earside = cfg['Parameters']['earside']
-            responder_device = cfg['Parameters']['responder_device']
-
-            return (device, attack, release, timeout, freqs, increase_5db,
-                    increase_10db, decrease_10db, decrease_20db,
-                    start_level_familiar, earside, responder_device)
-
+        self.device = args.device
+        self.responder_device = args.responder_device
+        self.attack = args.attack
+        self.release = args.release
+        self.timeout = args.timeout
+        self.earside = args.earside
+        self.small_level_increment = args.small_level_increment
+        self.large_level_increment = args.large_level_increment
+        self.small_level_decrement = args.small_level_decrement
+        self.large_level_decrement = args.large_level_decrement
+        self.start_level_familiar = args.start_level_familiar
+        self.freqs = [int(i) for i in args.freqs.split(',')]
