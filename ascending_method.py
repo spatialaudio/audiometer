4085fa754f5a7e414f776faa5c52e49774b1a36c
franzpl <franz.plocksties@uni-rostock.de>
2016-05-13 16:09:03 +0200

    add in/decrement-functions

--- ascending_method.py
+++ ascending_method.py
@@ -18,15 +18,30 @@ from audiometer import controller
 from audiometer import familiarization
 
 
-def step1(freq, increase_10db, decrease_20db, start_level_familiar, earside):
-    familiar_result = familiarization.fam(ctrl, freq, increase_10db,
-                                          decrease_20db, start_level_familiar,
-                                          earside)
-    current_level = familiar_result - decrease_10db
-    click = ctrl.process(freq, familiar_result, earside)
+def decrement_click(current_level, level_decrement):
+
+    current_level -= level_decrement
+    click = ctrl.process(freq, current_level, earside)
+    return current_level, click
+
+
+def increment_click(current_level, level_increment):
+
+    current_level += level_increment
+    click = ctrl.process(freq, current_level, earside)
+    return current_level, click
+
+
+def step1(freq, start_level_familiar, earside):
+    familiar_result = familiarization.fam(ctrl, freq, large_level_increment,
+                                          large_level_decrement,
+                                          start_level_familiar, earside)
+
+    current_level, click = decrement_click(familiar_result,
+                                           small_level_decrement)
+
     while not click:
-        current_level += increase_5db
-        click = ctrl.process(freq, current_level, earside)
+        increment_click(current_level, small_level_increment)
     return current_level, familiar_result, click
 
 
@@ -36,12 +51,11 @@ def step2(click, current_level, freq, earside):
         current_level_list = []
         for x in range(5):
             while click:
-                current_level -= decrease_10db
-                click = ctrl.process(freq, current_level, earside)
-
+                current_level, click = decrement_click(current_level,
+                                                       small_level_decrement)
             while not click:
-                current_level += increase_5db
-                click = ctrl.process(freq, current_level, earside)
+                current_level, click = increment_click(current_level,
+                                                       small_level_increment)
             current_level_list.append(current_level)
             # http://stackoverflow.com/a/11236055
             if [k for k in current_level_list
@@ -49,24 +63,33 @@ def step2(click, current_level, freq, earside):
                 three_answers = True
                 break
         else:
-            current_level += increase_10db
+            current_level += large_level_increment
         return current_level
 
 
 with controller.Controller() as ctrl:
-    freq_list = ctrl.get_freqs()
-    earside_order = ctrl.get_earside()
-    (increase_5db, increase_10db, decrease_10db,
-    decrease_20db) = ctrl.get_increases_decreases()
-    start_level_familiar = ctrl.get_start_level_familiar()
+
+    freq_list = ctrl.freqs
+
+    earside_order = ctrl.earside_order
+
+    small_level_increment = ctrl.small_level_increment
+    large_level_increment = ctrl.large_level_increment
+    small_level_decrement = ctrl.small_level_decrement
+    large_level_decrement = ctrl.large_level_decrement
+
+    start_level_familiar = ctrl.start_level_familiar
+
     for earside in earside_order:
-        for freq in freq_list:
-            current_level, familiar_result, click = step1(freq, increase_10db,
-                                                          decrease_20db,
+        i = 0
+        for freq in freq_list[i:]:
+            try:
+                current_level, familiar_result, click = step1(freq,
                                                           start_level_familiar,
                                                           earside)
-            current_level = step2(click, current_level, freq, earside)
-            ctrl.save_results(familiar_result, current_level, freq, earside)
-
-
+                current_level = step2(click, current_level, freq, earside)
+                ctrl.save_results(familiar_result, current_level, freq, earside)
+            except OverflowError:
+                jump_to_next_freq = freq_list.index(freq) + 1
+                i = jump_to_next_freq
 
