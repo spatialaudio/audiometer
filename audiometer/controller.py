607363b51af4704317e3a41d30d58f8c99e139b0
franzpl <franz.plocksties@uni-rostock.de>
2016-05-13 16:04:41 +0200

    use config as an object

--- audiometer/controller.py
+++ audiometer/controller.py
@@ -4,51 +4,56 @@ import csv
 import time
 import random
 from audiometer import config_loader
+import os
 
 
 class Controller:
 
     def __init__(self):
-        (device, attack, release, self.timeout, self.freqs, self.increase_5db,
-        self.increase_10db, self.decrease_10db, self.decrease_20db,
-        self.start_level_familiar, self.earside_beginning,
-        responder_device) = config_loader.load()
 
-        self._csvfile = open('results.csv', 'w')
-        self._writer = csv.writer(self._csvfile)
-        self._writer.writerow(['Familiarization Result / dB', 'Level / dB',
-                               'Frequency / Hz', 'Earside'])
+        self._cfg = config_loader.Config()
+        self.freqs = self._cfg.freqs
 
-        self._audio = tone_generator.AudioStream(device, attack, release)
-        self._rpd = responder.Responder(self.timeout, responder_device)
-
-    def get_increases_decreases(self):
-        return (self.increase_5db, self.increase_10db, self.decrease_10db,
-                self.decrease_20db)
-
-    def get_start_level_familiar(self):
-        return self.start_level_familiar
-
-    def get_freqs(self):
-        return self.freqs
-
-    def get_earside(self):
-        if self.earside_beginning == 'right':
+        if self._cfg.earside == 'right':
             earside_contralateral = 'left'
-        elif self.earside_beginning == 'left':
+        elif self._cfg.earside == 'left':
             earside_contralateral = 'right'
         else:
             raise ValueError("earside_beginning must be 'right' or 'left'")
-        return self.earside_beginning, earside_contralateral
+        self.earside_order = self._cfg.earside, earside_contralateral
+
+        self.small_level_increment = self._cfg.small_level_increment
+        self.large_level_increment = self._cfg.large_level_increment
+        self.small_level_decrement = self._cfg.small_level_decrement
+        self.large_level_decrement = self._cfg.large_level_decrement
+
+        self.start_level_familiar = self._cfg.start_level_familiar
+
+        path = 'audiometer/results'
+        if not os.path.exists(path):
+            os.makedirs(path)
+        filename = ('result_{}'.format(time.strftime("%d.%m.%Y_%H:%M:%S")) +
+                   '.csv')
+        self._csvfile = open(os.path.join(path, filename), 'w')
+        self._writer = csv.writer(self._csvfile)
+        self._writer.writerow(['Familiarization Result/dB', 'Level/dB',
+                               'Frequency/Hz', 'Earside'])
+
+        self._audio = tone_generator.AudioStream(self._cfg.device,
+                                                 self._cfg.attack,
+                                                 self._cfg.release)
+        self._rpd = responder.Responder(self._cfg.timeout,
+                                        self._cfg.responder_device)
 
     def process(self, freq, level, earside):
         if level >= 0:
             raise OverflowError("The signal is distorted. Possible causes are "
-            "an incorrect calibration or a severe hearing loss")
+            "an incorrect calibration or a severe hearing loss. I'm going "
+            "to the next frequency")
         self._audio.start(freq, level, earside)
         click = self._rpd.wait_for_click()
         self._audio.stop()
-        time.sleep(self.timeout + random.random())
+        time.sleep(self._cfg.timeout + random.random())
         return click
 
     def save_results(self, familiar_result, level, freq, earside):
