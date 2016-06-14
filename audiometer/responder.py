16add24d5fc8f7f67c4a3c89fb5187769d0d4784
franzpl <franz.plocksties@uni-rostock.de>
2016-06-14 15:12:33 +0200

    add new threads for click down and up and arrow keys

--- audiometer/responder.py
+++ audiometer/responder.py
@@ -9,37 +9,64 @@ class Responder:
 
     def __init__(self, timeout, responder_device):
         self._timeout = timeout
-        self._event = threading.Event()
-        self._event.set()
+        self._event1 = threading.Event()
+        self._event1.set()
+        self._event2 = threading.Event()
+        self._event2.set()
+        self._event3 = threading.Event()
+        self._event3.set()
         self._hookman = pyxhook.HookManager()
-        if responder_device == "mouse left":
-            self._hookman.MouseAllButtonsDown = self._mcevent
-        elif responder_device == 'spacebar':
-            self._hookman.KeyDown = self._kbevent
-        else:
-            raise ValueError("The string ist not valid")
+        self._hookman.MouseAllButtonsDown = self._mcevent_down
+        self._hookman.MouseAllButtonsUp = self._mcevent_up
+        self._hookman.KeyDown = self._kbevent
         self._hookman.start()
 
     def close(self):
         time.sleep(0.01)
         self._hookman.cancel()
 
-    def wait_for_click(self, timeout=None):
-        self._event.clear()
-        if timeout is None:
-            self._event.wait(timeout=self._timeout)
+    def click_down(self):
+        if self._event1.is_set() and not self._event3.is_set():
+            return True
         else:
-            self._event.wait(timeout=timeout)
+            return False
 
-        return self._event.is_set()
+    def click_up(self):
+        if self._event3.is_set():
+            return True
+        else:
+            return False
 
-    def _mcevent(self, event):
+    def clear(self):
+        self._event1.clear()
+        self._event3.clear()
+
+    def wait_for_arrow(self):
+        self._event2.clear()
+        self._event2.wait()
+        return self._key
+
+    def wait_for_click_up(self):
+        self._event3.wait()
+
+    def _mcevent_down(self, event):
         if event.MessageName == "mouse left down":
-            self._event.set()
+            self._event1.set()
+
+    def _mcevent_up(self, event):
+        if event.MessageName == "mouse left up":
+            self._event3.set()
 
     def _kbevent(self, event):
-        if event.MessageName == "key down" and event.Key == "space":
-            self._event.set()
+        if event.MessageName == "key down" and event.Key == "Down":
+            self._key = "arrow_down"
+            self._event2.set()
+        if event.MessageName == "key down" and event.Key == "Up":
+            self._key = "arrow_up"
+            self._event2.set()
+        if event.MessageName == "key down" and event.Key == "Return":
+            self._key = "enter"
+            self._event2.set()
 
     def __exit__(self, *args):
         self.close()
